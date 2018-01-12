1ce0abe2b69372e9025c95782f8b5670deb35746
zhangzhongfu <zhangzhongfu@sensetime.com>
2018-01-12 14:23:08 +0800

    add webview

--- app/src/main/java/name/arche/www/answerassistant/ui/WebviewActivity.java
+++ app/src/main/java/name/arche/www/answerassistant/ui/WebviewActivity.java
@@ -4,7 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 import name.arche.www.answerassistant.R;
 
@@ -29,8 +35,17 @@ public class WebviewActivity extends AppCompatActivity {
         Intent intent = getIntent();
         if (intent != null) {
             String question = intent.getStringExtra("question");
-            String url = HOST_NAME + question + "&rn=20";
+            if (TextUtils.isEmpty(question)){
+                question = "Arche";
+            }
+            String url = HOST_NAME;
+            try {
+                url = url + URLEncoder.encode(question, "gb2312") + "&rn=20";
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
             mWebView.loadUrl(url);
+            mWebView.setWebViewClient(new WebViewClient());
         }
     }
 
